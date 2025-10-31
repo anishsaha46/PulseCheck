a066f74d5e395db1d940614dc4e8a40d886c7faf
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-10-31 20:31:01 +0530

    added auth service for Login

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,26 +1,55 @@
 import bcrypt from "bcryptjs"
-import {generateToken} from "../config/auth";
-import prisma from "../config/database";
+import { generateToken } from "../config/auth"
+import prisma from "../config/database"
 
-export const authService= async(data:{email:string; password:string; name:string}) => {
-
+export const authService = {
+  async register(data: { email: string; password: string; name: string }) {
     const existingUser = await prisma.user.findUnique({
-        where:{email:data.email}
+      where: { email: data.email },
     })
-    if(existingUser){
-        throw new Error("User already exists")
-    }
-    const hashedPassword = await bcrypt.hash(data.password,10)
-    const user = await prisma.user.create({
-        data:{
-            email:data.email,
-            password:hashedPassword,
-            name:data.name
-        }
-    })
-    const token =generateToken(user.id)
-    return{
-        user:{id:user.id,email:user.email,name:user.email},token,
-    }
-}
 
+    if (existingUser) {
+      throw new Error("User already exists")
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10)
+
+    const user = await prisma.user.create({
+      data: {
+        email: data.email,
+        name: data.name,
+        password: hashedPassword,
+      },
+    })
+
+    const token = generateToken(user.id)
+
+    return {
+      user: { id: user.id, email: user.email, name: user.name },
+      token,
+    }
+  },
+
+  async login(data: { email: string; password: string }) {
+    const user = await prisma.user.findUnique({
+      where: { email: data.email },
+    })
+
+    if (!user) {
+      throw new Error("Invalid credentials")
+    }
+
+    const passwordMatch = await bcrypt.compare(data.password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error("Invalid credentials")
+    }
+
+    const token = generateToken(user.id)
+
+    return {
+      user: { id: user.id, email: user.email, name: user.name },
+      token,
+    }
+  },
+}
