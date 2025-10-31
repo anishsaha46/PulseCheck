ff3879d62966bbee023767c0380984390274ac11
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-10-31 14:48:23 +0530

    feat(auth): add empty auth configuration file

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken"
+import {config} from './config'
+
+export const generateToken = (userId:string):string => {
+    return jwt.sign({userId},config.JWT_SECRET,{expiresIn:"7d"})
+}
+
+export const verifyToken=(token:string):string | null => {
+    try{
+        const decoded = jwt.verify(token,config.JWT_SECRET) as {userId:string}
+        return decoded.userId
+    } catch{
+        return null;
+    }
+}
