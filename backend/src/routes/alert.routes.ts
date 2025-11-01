512a954801dc2a40b384768d9791dd3dba90174f
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-11-01 06:51:09 +0530

    feat(routes): add alert routes with CRUD operations

---
+++ backend/src/routes/alert.routes.ts
@@ -0,0 +1,15 @@
+import { Router,type Request,type Response } from "express";
+import { authMiddleware } from "../middleware/auth.middleware";
+import { authController } from "../controllers/auth.controller";
+import { alertController } from "../controllers/alert.controller";
+
+const router = Router()
+router.use(authMiddleware)
+
+router.get("/",(req:Request,res:Response)=> alertController.getAlerts(req,res))
+
+router.post("/",(req:Request,res:Response)=>alertController.createAlert(req,res))
+
+router.put("/:id",(req:Request,res:Response)=>alertController.updateAlert(req,res))
+
+router.delete("/:id",(req:Request,res:Response)=>alertController.deleteAlert(req,res))
