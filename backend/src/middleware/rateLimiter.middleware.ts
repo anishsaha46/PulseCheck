c1607562848ac17abb36bbe6ae64faaf2c08fc77
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-11-04 12:33:05 +0530

    fix(rateLimiter): adjust rate limit window from 16 to 15 minutes

--- backend/src/middleware/rateLimiter.middleware.ts
+++ backend/src/middleware/rateLimiter.middleware.ts
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit"
 
 export const apiLimiter = rateLimit({
-    windowMs:16*60*1000,
+    windowMs:15*60*1000,
     max:100,
     message:"Too many requests from this IP , please try again later",
 })
